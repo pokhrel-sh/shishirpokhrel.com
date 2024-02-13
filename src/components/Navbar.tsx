c23df35ef25473486ce99e32293eaf7e7719e86c
Shishir Pokhrel <pokhrel.sh@northeastern.edu>
2024-02-13 15:36:12 -0800

    fixed bugs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,6 @@ const links =[
   { to: "/", text: "Home", color: "bg-blue-300"},
   { to: "/skills", text: "Skills", color: "bg-green-300"},
   { to: "/projects", text: "Projects", color: "bg-yellow-300"},
-  { to: "../public/shishirResume2.pdf", text: "Resume", color: "bg-red-300", newTab: true},
 ]
 
 
@@ -45,14 +44,14 @@ function Navbar() {
           {showOptions && (
             <div>
               {links.map((link, index) => (
-                link.newTab ?
-                <a key= {index} href={link.to} target = "_blank" rel = "noopener referrer" className = {`${link.color} m-1 hover:bg-yellow-100 text-black font-bold py-2 px-4 rounded`} onClick={handleLinkClick}>
-                  {link.text}
-                </a>:
                 <Link key={index} to={link.to} className={`${link.color} m-1 hover:bg-yellow-100 text-black font-bold py-2 px-4 rounded`} onClick={handleLinkClick}>
                   {link.text}
                 </Link>
               ))}
+              <button
+                className="flex flex-col px-4 py-2 border rounded bg-green-100">
+                <a href="../public/shishirResume2.pdf" target="blank" bg-blue-300>Resume</a>
+              </button>
             </div>
           )}
         </div>
