0a971c41502fd055779c08c7731ad14fbea4994c
Shishir Pokhrel <pokhrel.sh@northeastern.edu>
2024-03-29 13:01:26 -0700

    add code to projects, and fixed navbar

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,9 +1,45 @@
 function Projects() {
   return (
-    <div>
-      <p>Projects</p>
+    <div className="grid grid-cols-2 gap-4">
+      <div className="bg-white rounded-md shadow-lg p-4">
+        <h2 className="text-lg font-bold mb-2"><a href="http://shishirpokhrel.com" className="hover:text-blue-500">Portfolio Site</a></h2>
+        <p className="text-gray-500">A typical portfolio website I created for fun, and to learn.</p>
+        <p className="text-blue-500 mt-2"><a href="https://github.com/shishirpokhrel/portfolio-site" target="blank">Github</a></p>
+        <p className="text-gray-500">Frameworks: React.js, Typescript, TailwindCSS</p>
+      </div>
+      <div className="bg-white rounded-md shadow-lg p-4">
+        <h2 className="text-lg font-bold mb-2"><a href="https://github.com/shishirpokhrel/crypto-web-app" className="hover:text-blue-500">BiblioConnect</a></h2>
+        <p className="text-gray-500">A web app that where you choose books you like based on first impression, and we recommend books based on that.</p>
+        <p className="text-blue-500 mt-2"><a href="https://github.com/Biblio-Connect/BiblioConnect" target="blank">Github</a></p>
+        <p className="text-gray-500">Frameworks: React.js, SQLite, Node.js, Typescript, TailwindCSS</p>
+      </div>
+      <div className="bg-white rounded-md shadow-lg p-4">
+        <h2 className="text-lg font-bold mb-2"><a href="https://github.com/pokhrel-sh/" className="hover:text-blue-500">Coming Soon</a></h2>
+        <p className="text-gray-500">The project will be coming soon!</p>
+        <p className="text-blue-500 mt-2"><a href="https://github.com/pokhrel-sh/" target="blank">Github</a></p>
+        <p className="text-gray-500">Frameworks: Frameworks will be coming soon!</p>
+      </div>
+      <div className="bg-white rounded-md shadow-lg p-4">
+        <h2 className="text-lg font-bold mb-2"><a href="https://github.com/pokhrel-sh/" className="hover:text-blue-500">Coming Soon</a></h2>
+        <p className="text-gray-500">The project will be coming soon!</p>
+        <p className="text-blue-500 mt-2"><a href="https://github.com/pokhrel-sh/" target="blank">Github</a></p>
+        <p className="text-gray-500">Frameworks: Frameworks will be coming soon!</p>
+      </div>
+      <div className="bg-white rounded-md shadow-lg p-4">
+      <h2 className="text-lg font-bold mb-2"><a href="https://github.com/pokhrel-sh/" className="hover:text-blue-500">Coming Soon</a></h2>
+      <p className="text-gray-500">The project will be coming soon!</p>
+      <p className="text-blue-500 mt-2"><a href="https://github.com/pokhrel-sh/" target="blank">Github</a></p>
+      <p className="text-gray-500">Frameworks: Frameworks will be coming soon!</p>
+    </div>
+    <div className="bg-white rounded-md shadow-lg p-4">
+        <h2 className="text-lg font-bold mb-2"><a href="https://github.com/pokhrel-sh/" className="hover:text-blue-500">Coming Soon</a></h2>
+        <p className="text-gray-500">The project will be coming soon!</p>
+        <p className="text-blue-500 mt-2"><a href="https://github.com/pokhrel-sh/" target="blank">Github</a></p>
+        <p className="text-gray-500">Frameworks: Frameworks will be coming soon!</p>
+      </div>
     </div>
   );
 }
+
 
 export default Projects;
