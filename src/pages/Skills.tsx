b8d00c42731252d228d0efc0858991a1fd3a4ca3
Shishir Pokhrel <shishirpokhrel@Shishirs-MacBook-Pro.local>
2024-09-19 23:24:17 -0400

    update skils

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -2,6 +2,9 @@ function Skills() {
   return (
     <div>
       <p>Skills</p>
+      <p>
+        I basically have no skills right now, I am in a process of revamping this website!
+      </p>
     </div>
   );
 }
