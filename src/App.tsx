b57859c7ee9ee0e766b48e79d5b860be7faf0687
Shishir Pokhrel <pokhrel.sh@northeastern.edu>
2024-10-18 00:11:09 -0400

    comment out stuff for netlify deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 // import Projects from "./pages/Projects";
 // import Skills from "./pages/Skills";
@@ -14,9 +14,7 @@ import Home from "./pages/Home";
 
 const App: React.FC = () => {
   return (
-    <Router>
       <Home />
-    </Router>
   );
 };
 
