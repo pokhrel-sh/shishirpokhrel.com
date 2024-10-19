a69b7da7c9b5cd3de3724d15bf1c9e855b8ecdb1
Shishir Pokhrel <pokhrel.sh@northeastern.edu>
2024-10-19 13:10:08 -0400

    update websote

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,6 @@
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
+
 
 function Home() {
 
@@ -38,7 +41,7 @@ function Home() {
         "name": "Portfolio Site",
         "description": "A project for this portfolio site",
         "status": "Always In Progress",
-        "github": "https://github.com/shishirpokhrel/portfolio-site",
+        "github": "https://github.com/pokhrel-sh/shishirpokhrel.com",
         "tech": "React.js, Typescript, TailwindCSS",
         "link": "https://shishirpokhrel.com",
         "notes": "",
@@ -55,7 +58,7 @@ function Home() {
         "githubLinkable": "Github"
       }, 
       {
-        "name": "HowMuchCanIStoleFromAmazon",
+        "name": "HowMuchIStoleFromAmazon",
         "description": "Shows how much I stole from Amazon",
         "status": "In Progress (Early stages)",
         "github": "",
@@ -79,24 +82,24 @@ function Home() {
 
   const currentClasses = [
     {
-      "name": "CS4520",
-      "description": "Mobile Development",
+      "id": "CS4520",
+      "name": "Mobile Development",
       "tech": "Swift, Firebase"
     },
     {
-      "name": "CS4530",
-      "description": "Software Engineering",
+      "id": "CS4530",
+      "name": "Software Engineering",
       "tech": "TypeScript, React"
     },
     {
-      "name": "CS4550",
-      "description": "Web Development",
+      "id": "CS4550",
+      "name": "Web Development",
       "tech": "TypeScript, React, Bootstrap"
     },
     {
-      "name": "THTR1125",
-      "description": "Improvisation",
-      "tech": ""
+      "id": "THTR1125",
+      "name": "Improvisation",
+      "tech": "Talking to people"
     }
   ]
 
@@ -126,17 +129,27 @@ function Home() {
   return (
 
 
-    <div>
-      <div>Shishir Pokhrel</div>
+    <div className="place-content-center flex-auto">
+      <div className="bg-gray-200 ">
+      <div className="text-3xl ">Shishir Pokhrel</div>
       <div>Computer Science @ Northeastern University</div>
-      <div>
+      </div>
+      <div className="bg-gray-200 mx-60 text-center">
         <div className="mt-8">About me</div>
-        <div>
-          I am an aspiring Software Engineer / Developer. I am currently interested in 
-          Web Development, Mobile Development. 
+        <div> My name is Shishir Pokhrel. I am a sophomore at Northeastern University studying Computer Science. I love learning new technologies everyday and have been exploring a lot of the things recently. I just bought a mini pc to use it as a server to test out dockers and other cool self-hosted projects. I am taking classes such as Software Engineering, Mobile Developement, and Web Developement. I am currently looking for a software engineering opportunities for Spring & Summer 2025. I like watching and playing Tennis (Kygrios is my Favorite player). I like walking around alone at 10pm in the night, cus why not
+          <div>
+            <div>Connect with me: </div>
+            <div>
+              <div className="text-center">
+              <a href="https://www.linkedin.com/in/shishir-pokhrel/"><FaLinkedin /></a>
+              <a href="https://www.github.com/pokhrel-sh"><FaGithub /></a>
+              </div>
+          </div>
+
         </div>
       </div>
-      <div>
+      </div>
+      <div className="my-8 mx-16 text-center bg-gray-300">
         <div>Skills and Technologies I've used: </div>
         <div>Languages: Java, JavaScript, TypeScript, SQL, Swift, Kotlin, HTML, CSS, C, C++ (unit testing)</div>
         <div>Frameworks: JUnit 5, React, Node.js, Express.js, Tailwind CSS, Bootstrap</div>
@@ -146,12 +159,12 @@ function Home() {
 
       <div>
         <div className="mt-8 mb-5">Current Classes</div>
-        <div>
+        <div className="grid grid-cols-2 border-2 bg-gray-200">
           {currentClasses.map((classes) => {
             return (
-              <div>
+              <div className="border-2 bg-gray-50 flex-auto">
+                <div>{classes.id}  - {classes.name} </div>
                 <div>{classes.name}</div>
-                <div>{classes.description}</div>
                 <div className="mb-4">{classes.tech}</div>
               </div>
             )
@@ -162,22 +175,18 @@ function Home() {
 
       
       <div>Projects</div>
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-4 bg-gray-100 flex-auto">
           {project.projects.map((projects) => {
             return (
-
-              <div
-                key={projects.name}
-                className="border-2 pr-2 pl-4 mt-4 mb-4 bg-gray-100 rounded-md shadow-lg"
-              >
-                <button className="bg-gray-200 mt-4"><a href={projects.link}>{projects.name}</a></button>
+                <div className="bg-gray-200 px-4 my-4">
+                <button className="bg-gray-200 pt-5 text-3xl hover:text-blue-800"><a href={projects.link}>{projects.name}</a><span className="text-sm">&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;{projects.tech}</span></button>
                 <div>{projects.description}</div>
                 <div>{projects.status}</div>
-                <button className="bg-gray-200"><a href={projects.github}>{projects.githubLinkable}</a></button>
+                <button className="bg-gray-300 hover:text-blue-800"><a href={projects.github}>{projects.githubLinkable}</a></button>
                 <div>{projects.tech}</div>
                 <div>{projects.link}</div>
-                <div>{projects.notes}</div>
-              </div>
+                <div className="pb-5">{projects.notes}</div>
+                </div>
             );
           })}
         </div>
